cc67a82c0d634b514af988937b11fb0ce9f214f6
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-10 22:56:08 +0000

    Updated blocks to use metals helper

--- src/main/java/dev/perxenic/stannum/registry/block/StannumBlocks.java
+++ src/main/java/dev/perxenic/stannum/registry/block/StannumBlocks.java
@@ -1,7 +1,7 @@
 package dev.perxenic.stannum.registry.block;
 
-import com.simibubi.create.foundation.data.recipe.CommonMetal;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import dev.perxenic.stannum.registry.StannumMetals;
 import dev.perxenic.stannum.registry.item.StannumItems;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
@@ -23,75 +23,84 @@ import static com.simibubi.create.foundation.data.TagGen.tagBlockAndItem;
 import static dev.perxenic.stannum.Stannum.REGISTRATE;
 
 public class StannumBlocks {
-    public static BlockEntry<Block> TIN_ORE = REGISTRATE.block("tin_ore", Block::new)
-            .initialProperties(() -> Blocks.COPPER_ORE)
-            .transform(pickaxeOnly())
-            .loot((lt, b) -> {
-                HolderLookup.RegistryLookup<Enchantment> enchantLookup = lt.getRegistries().lookupOrThrow(Registries.ENCHANTMENT);
-                Holder<Enchantment> fortune = enchantLookup.getOrThrow(Enchantments.FORTUNE);
+    public static BlockEntry<Block> TIN_ORE;
+    public static BlockEntry<Block> DEEPSLATE_TIN_ORE;
+    public static BlockEntry<Block> RAW_TIN_BLOCK;
+    public static BlockEntry<Block> TIN_BLOCK;
 
-                lt.add(b,
-                        lt.createSilkTouchDispatchTable(b,
-                                lt.applyExplosionDecay(b, LootItem.lootTableItem(StannumItems.RAW_TIN))
-                                        .apply(ApplyBonusCount.addOreBonusCount(fortune))
-                        )
-                );
-            })
-            .tag(BlockTags.NEEDS_IRON_TOOL)
-            .transform(tagBlockAndItem(Map.of(
-                    CommonMetal.TIN.ores.blocks(), CommonMetal.TIN.ores.items(),
-                    Tags.Blocks.ORES_IN_GROUND_STONE, Tags.Items.ORES_IN_GROUND_STONE
-            )))
-            .build()
-            .register();
 
-    public static BlockEntry<Block> DEEPSLATE_TIN_ORE = REGISTRATE.block("deepslate_tin_ore", Block::new)
-            .initialProperties(() -> Blocks.DEEPSLATE_COPPER_ORE)
-            .transform(pickaxeOnly())
-            .loot((lt, b) -> {
-                HolderLookup.RegistryLookup<Enchantment> enchantLookup = lt.getRegistries().lookupOrThrow(Registries.ENCHANTMENT);
-                Holder<Enchantment> fortune = enchantLookup.getOrThrow(Enchantments.FORTUNE);
-
-                lt.add(b,
-                        lt.createSilkTouchDispatchTable(b,
-                                lt.applyExplosionDecay(b, LootItem.lootTableItem(StannumItems.RAW_TIN))
-                                        .apply(ApplyBonusCount.addOreBonusCount(fortune))
-                        )
-                );
-            })
-            .tag(BlockTags.NEEDS_IRON_TOOL)
-            .transform(tagBlockAndItem(Map.of(
-                    CommonMetal.TIN.ores.blocks(), CommonMetal.TIN.ores.items(),
-                    Tags.Blocks.ORES_IN_GROUND_DEEPSLATE, Tags.Items.ORES_IN_GROUND_DEEPSLATE
-            )))
-            .build()
-            .register();
-
-    public static BlockEntry<Block> RAW_TIN_BLOCK = REGISTRATE.block("raw_tin_block", Block::new)
-            .initialProperties(() -> Blocks.RAW_COPPER_BLOCK)
-            .properties(p -> p.mapColor(MapColor.METAL))
-            .transform(pickaxeOnly())
-            .lang("Block of Raw Tin")
-            .tag(BlockTags.NEEDS_IRON_TOOL)
-            .transform(tagBlockAndItem(Map.of(
-                    CommonMetal.TIN.rawStorageBlocks.blocks(), CommonMetal.TIN.rawStorageBlocks.items()
-            )))
-            .build()
-            .register();
-
-    public static BlockEntry<Block> TIN_BLOCK = REGISTRATE.block("tin_block", Block::new)
-            .initialProperties(() -> Blocks.COPPER_BLOCK)
-            .properties(p -> p.mapColor(MapColor.METAL))
-            .transform(pickaxeOnly())
-            .lang("Block of Tin")
-            .tag(BlockTags.NEEDS_IRON_TOOL)
-            .transform(tagBlockAndItem(Map.of(
-                    CommonMetal.TIN.storageBlocks.blocks(), CommonMetal.TIN.storageBlocks.items()
-            )))
-            .build()
-            .register();
-
-    // Load class
     public static void register() {
+        // Tin is known to be natural so ores and raw storage blocks will never be null
+        assert StannumMetals.TIN.natural;
+        assert StannumMetals.TIN.ores != null;
+        assert StannumMetals.TIN.rawStorageBlocks != null;
+
+        TIN_ORE = REGISTRATE.block("tin_ore", Block::new)
+                .initialProperties(() -> Blocks.COPPER_ORE)
+                .transform(pickaxeOnly())
+                .loot((lt, b) -> {
+                    HolderLookup.RegistryLookup<Enchantment> enchantLookup = lt.getRegistries().lookupOrThrow(Registries.ENCHANTMENT);
+                    Holder<Enchantment> fortune = enchantLookup.getOrThrow(Enchantments.FORTUNE);
+
+                    lt.add(b,
+                            lt.createSilkTouchDispatchTable(b,
+                                    lt.applyExplosionDecay(b, LootItem.lootTableItem(StannumItems.RAW_TIN))
+                                            .apply(ApplyBonusCount.addOreBonusCount(fortune))
+                            )
+                    );
+                })
+                .tag(BlockTags.NEEDS_IRON_TOOL)
+                .transform(tagBlockAndItem(Map.of(
+                        StannumMetals.TIN.ores.blocks(), StannumMetals.TIN.ores.items(),
+                        Tags.Blocks.ORES_IN_GROUND_STONE, Tags.Items.ORES_IN_GROUND_STONE
+                )))
+                .build()
+                .register();
+
+        DEEPSLATE_TIN_ORE = REGISTRATE.block("deepslate_tin_ore", Block::new)
+                .initialProperties(() -> Blocks.DEEPSLATE_COPPER_ORE)
+                .transform(pickaxeOnly())
+                .loot((lt, b) -> {
+                    HolderLookup.RegistryLookup<Enchantment> enchantLookup = lt.getRegistries().lookupOrThrow(Registries.ENCHANTMENT);
+                    Holder<Enchantment> fortune = enchantLookup.getOrThrow(Enchantments.FORTUNE);
+
+                    lt.add(b,
+                            lt.createSilkTouchDispatchTable(b,
+                                    lt.applyExplosionDecay(b, LootItem.lootTableItem(StannumItems.RAW_TIN))
+                                            .apply(ApplyBonusCount.addOreBonusCount(fortune))
+                            )
+                    );
+                })
+                .tag(BlockTags.NEEDS_IRON_TOOL)
+                .transform(tagBlockAndItem(Map.of(
+                        StannumMetals.TIN.ores.blocks(), StannumMetals.TIN.ores.items(),
+                        Tags.Blocks.ORES_IN_GROUND_DEEPSLATE, Tags.Items.ORES_IN_GROUND_DEEPSLATE
+                )))
+                .build()
+                .register();
+
+        RAW_TIN_BLOCK = REGISTRATE.block("raw_tin_block", Block::new)
+                .initialProperties(() -> Blocks.RAW_COPPER_BLOCK)
+                .properties(p -> p.mapColor(MapColor.METAL))
+                .transform(pickaxeOnly())
+                .lang("Block of Raw Tin")
+                .tag(BlockTags.NEEDS_IRON_TOOL)
+                .transform(tagBlockAndItem(Map.of(
+                        StannumMetals.TIN.rawStorageBlocks.blocks(), StannumMetals.TIN.rawStorageBlocks.items()
+                )))
+                .build()
+                .register();
+
+        TIN_BLOCK = REGISTRATE.block("tin_block", Block::new)
+                .initialProperties(() -> Blocks.COPPER_BLOCK)
+                .properties(p -> p.mapColor(MapColor.METAL))
+                .transform(pickaxeOnly())
+                .lang("Block of Tin")
+                .tag(BlockTags.NEEDS_IRON_TOOL)
+                .transform(tagBlockAndItem(Map.of(
+                        StannumMetals.TIN.storageBlocks.blocks(), StannumMetals.TIN.storageBlocks.items()
+                )))
+                .build()
+                .register();
     }
 }
